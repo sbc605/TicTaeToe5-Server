ccddc5c36140c8a6b2a16d9947c749f95bdb6af4
sbc605 <sbc605@naver.com>
2025-09-09 18:21:20 +0900

    disconnect 구현

--- game.js
+++ game.js
@@ -59,5 +59,12 @@ module.exports = function(server) {
             console.log('Player action in room:', roomId, 'Block index:', blockIndex);
             socket.to(roomId).emit('doOpponent', { blockIndex: blockIndex });
         });
+
+        // socket.on('disconnect', function(reason){
+        //     console.log('Disconnected:' + socket.id + 'Reason:' + reason);
+
+         socket.on("disconnect", (reason) => {
+             console.log('Disconnected:' + socket.id + 'Reason:' + reason);
+        });
     });
 };
